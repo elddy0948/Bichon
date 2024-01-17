cbf9293788b5ed8ec23c237e6f2bed139c816014
Joons <elddy0948@gmail.com>
2024-01-17 14:16:59 +0900

    feat: recording video

--- CameraWithCL/CameraWithCL/main.cpp
+++ CameraWithCL/CameraWithCL/main.cpp
@@ -8,7 +8,7 @@ using namespace cv;
 
 int main(void) 
 {
-	ocl::setUseOpenCL(true);
+	//ocl::setUseOpenCL(true);
 
 	VideoCapture capture(0);
 
@@ -18,18 +18,42 @@ int main(void)
 		return -1;
 	}
 
-	UMat img;
+	int w = cvRound(capture.get(CAP_PROP_FRAME_WIDTH));
+	int h = cvRound(capture.get(CAP_PROP_FRAME_HEIGHT));
+	double fps = capture.get(CAP_PROP_FPS);
+
+	int fourcc = VideoWriter::fourcc('D', 'I', 'V', 'X');
+	int delay = cvRound(1000 / 60);
+
+	cout << delay << '\n';
+
+	VideoWriter outputVideo;
+
+	outputVideo.open("opt.avi", fourcc, 60.0, Size(w, h));
+	
+	if (!outputVideo.isOpened()) {
+		cout << "File open failed!\n" << endl;
+		return -1;
+	}
+
+	Mat img;
+
 	while (1)
 	{
 		capture >> img;
-		UMat dst;
+		if (img.empty()) { break; }
+
+		Mat dst;
 
 		/* Use Gaussian blur */
 		int sigma = 5;
 		GaussianBlur(img, dst, Size(), (double)sigma);
+
+		outputVideo << dst;
+
 		imshow("img", dst);
 
-		if (waitKey(1) == 27) { break; }
+		if (waitKey(30) == 27) { break; }
 	}
 
 	destroyAllWindows();
